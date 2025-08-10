ddd63356a314a8adb7a97f7380756d305ea114e5
atmoner <contact.atmoner@gmail.com>
2025-08-10 11:54:54 +0200

    Bump version to 1.0.25 in package.json and import useRoute and navigateTo in [id].vue for improved routing functionality

--- src/runtime/pages/admin/blog/edit/[id].vue
+++ src/runtime/pages/admin/blog/edit/[id].vue
@@ -433,6 +433,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useRoute, navigateTo } from '#app'
 import { useAuth } from '../../../composables/useAuth'
 import { useBlog } from '../../../composables/useBlog'
 
